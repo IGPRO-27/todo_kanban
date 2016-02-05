9d3d8acca7c10a35b842947b5e0fa8a8f63e03a2
Adel27 <r-27adel@live.fr>
2016-02-05 18:05:22 +0100

    Add view for custom CSS and JS assets

--- __openerp__.py
+++ __openerp__.py
@@ -3,5 +3,8 @@
     'description': 'Kanban board for to-do tasks.',
     'author': 'igpro',
     'depends': ['todo_ui'],
-    'data': ['todo_kanban_view.xml'] 
+    'data': [
+        'todo_kanban_view.xml',
+        # 'views/todo_kanban_assets.xml'
+    ]
 }
